c0c742faf07c31d35a67460c823e98c7f4cd0052
chengdu <3281328128@qq.com>
2019-08-05 21:18:56 +0800

    upate keyutil

--- src/main/java/com/scd/util/KeyUtil.java
+++ src/main/java/com/scd/util/KeyUtil.java
@@ -9,76 +9,61 @@ import java.util.UUID;
  */
 public class KeyUtil {
 
-    private static String[][] ENCRYPT_ELEMENTS = {
-            new String[]{"~","!","@","#","$","%","^","&","*","(",")","_","+","{","}",":",",",".","?"},
-            new String[]{"`","1","2","3","4","5","6","7","8","9","0","-","=","[","]",";","<",">","/"},
-            new String[]{"A","B","C","D","E","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U",
-                    "V","W","X","Y","Z"},
-            new String[]{"a","b","c","d","e","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u",
-                    "v","w","x","y","z"}
+    private static char[][] ENCRYPT_ELEMENTS = {
+            new char[]{'~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '{', '}', ':', ',', '.', '?','|'},
+            new char[]{'`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '[', ']', ';', '<', '>', '/', '\\'},
+            new char[]{'A', 'B', 'C', 'D', 'E', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+                    'V', 'W', 'X', 'Y', 'Z'},
+            new char[]{'a', 'b', 'c', 'd', 'e', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+                    'v', 'w', 'x', 'y', 'z'}
     };
 
     private static class SecureRandomHolder {
         static final SecureRandom random = new SecureRandom();
     }
 
-	public static String genRandomKey(int keyLength) {
-		StringBuilder key = new StringBuilder();
-        String strKey;
+    public static String genRandomKey(int keyLength) {
+        StringBuilder key = new StringBuilder();
+        char strKey;
         int index;
-		for (int i = 0; i < keyLength; i++) {
-			// 产生随机二位数组索引 0-3
+        for (int i = 0; i < keyLength; i++) {
+            // 产生随机二位数组索引 0-3
             index = SecureRandomHolder.random.nextInt(4);
-			switch (index){
-				case 0:
-				    // 第一个二位数组索引
+            switch (index) {
+                case 0:
+                    // 第一个二位数组索引
                     index = SecureRandomHolder.random.nextInt(ENCRYPT_ELEMENTS[0].length);
                     strKey = ENCRYPT_ELEMENTS[0][index];
                     key.append(strKey);
-					break;
-				case 1:
-				    // 第二个二位数组索引
+                    break;
+                case 1:
+                    // 第二个二位数组索引
                     index = SecureRandomHolder.random.nextInt(ENCRYPT_ELEMENTS[1].length);
-					strKey = ENCRYPT_ELEMENTS[1][index];
-					key.append(strKey);
-					break;
-				case 2:
-					// 第三个二位数组索引
+                    strKey = ENCRYPT_ELEMENTS[1][index];
+                    key.append(strKey);
+                    break;
+                case 2:
+                    // 第三个二位数组索引
                     index = SecureRandomHolder.random.nextInt(ENCRYPT_ELEMENTS[2].length);
-					strKey = ENCRYPT_ELEMENTS[2][index];
-					key.append(strKey);
-					break;
+                    strKey = ENCRYPT_ELEMENTS[2][index];
+                    key.append(strKey);
+                    break;
                 case 3:
                     index = SecureRandomHolder.random.nextInt(ENCRYPT_ELEMENTS[3].length);
                     strKey = ENCRYPT_ELEMENTS[3][index];
                     key.append(strKey);
-				default:
-					break;
-			}
-		}
-		return key.toString();
-	}
-
-	public static String genUidKey(int keyLength){
-	    if (keyLength > 36){
-	        throw new IllegalArgumentException("key length is too Long");
+                default:
+                    break;
+            }
         }
-	    String uuid = UUID.randomUUID().toString();
+        return key.toString();
+    }
+
+    public static String genUidKey(int keyLength) {
+        if (keyLength > 36) {
+            throw new IllegalArgumentException("key length is too Long");
+        }
+        String uuid = UUID.randomUUID().toString();
         return uuid.substring(0, keyLength);
-    }
-
-    private static void convertToLower(){
-        String[] en = ENCRYPT_ELEMENTS[2];
-        StringBuilder stringBuilder = new StringBuilder("");
-        for (String s : en){
-            stringBuilder.append("\"").append(s.toLowerCase()).append("\"")
-                    .append(",");
-        }
-        String str = stringBuilder.toString();
-        System.out.println(str.substring(0, str.length() - 1));
-    }
-
-    public static void main(String[] args){
-        convertToLower();
     }
 }
